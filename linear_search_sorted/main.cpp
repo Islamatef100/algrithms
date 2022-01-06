b0be626b5c48c3f78ea38a6fcca100c2c9387959
islam atef nagy <78918678+Islamatef100@users.noreply.github.com>
2022-01-06 17:48:40 +0200

    Add files via upload

---
+++ linear_search_sorted/main.cpp
@@ -0,0 +1,40 @@
+//linear search sorted.
+/* the diference between sorted or not that the sorted linear search id more smart becouse i will check if ellement==arr[i] and if element>arr[i] becouse if element>arr[] then element not xist ant return from serch.*/
+#include <iostream>
+using namespace std;
+template<class Newtype>
+bool linear_search(Newtype arr[],int size,Newtype data)
+{
+    int i=0;
+   // bool exist_or_not=false;
+    for(i;;i++)
+    /* i removed i<size becouse i added the element in the end of array by whn i creat array iincreases the size by 1 and in for loab it will  fint the ellement after for loap i check if i==size the the code found the element which i added it so the element in this condition will not exist.*/
+    {
+        if(arr[i]==data)
+        {  // exist_or_not=true;
+            break;
+        }
+        if(data<arr[i])
+        {
+            i=size;//to make algorithm know that this ellement not exist.
+            break;
+        }
+    }
+    return 1 ?i!=size:0;// i mean here if(i!=size)return 1 else return 0;
+}
+int main()
+{
+    int size,data=0;
+    cout<<"enter size of array:";
+    cin>>size;
+    int *arr=new int[size+1];
+    for(int i=0;i<size;i++)
+    cin>>arr[i];
+    cout<<"enter the ellement:";
+    cin>>data;
+    arr[size]=data;
+  if(linear_search(arr,size,data))
+      cout<<"the ellement exist:\n";
+    else
+        cout<<"the ellement not exist:\n";
+}
